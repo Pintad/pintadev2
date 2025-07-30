ffad2f9ad21621a3a0aa7c9ab0941f3be6feab7d
Pintade <155299891+Pintad@users.noreply.github.com>
2025-07-30 15:28:41 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,9 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://rmwojbenogfywrwanhpk.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtd29qYmVub2dmeXdyd2FuaHBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5ODQ4NDcsImV4cCI6MjA2MDU2MDg0N30.imQy9hiT33ri0UbzSrYP31M2_kTis4p0S-FurPZNrX8";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
